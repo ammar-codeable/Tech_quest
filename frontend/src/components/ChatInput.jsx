a188a7b813f94aa8a6ad183ff0e3b8eda1657de5
aaa <aaa>
2024-04-05 20:52:42 -0700

    Chatbox

--- frontend/src/components/ChatInput.jsx
+++ frontend/src/components/ChatInput.jsx
@@ -1,4 +1,13 @@
 export default function ChatInput() {
+  const handleKeyDown = (event) => {
+    if(event.key === 'Enter' && event.shiftKey) {
+      event.preventDefault();
+
+      console.log(message);
+      setMessage('');
+    }
+  }
+
   return (
     <div className="bottom-0 w-full p-4">
       <label
@@ -10,9 +19,11 @@ export default function ChatInput() {
       <textarea
         id="message"
         rows="4"
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}
         className="block p-2.5  bottom-0 w-full h-20 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Start Chatting"
-        onEnter
+        onKeyDown={handleKeyDown}
       ></textarea>
     </div>
   );
